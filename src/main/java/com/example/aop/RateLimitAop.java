a4fe2b6cc0c203c0a6fde27235ffcbd2feb6d001
Sakura <360736293@qq.com>
2023-11-15 17:06:21 +0800

    update：优化限流的AOP逻辑；

--- src/main/java/com/example/aop/RateLimitAop.java
+++ src/main/java/com/example/aop/RateLimitAop.java
@@ -1,11 +1,11 @@
 package com.example.aop;
 
-import cn.hutool.core.util.ObjectUtil;
 import com.example.annotation.RateLimit;
 import com.example.exception.RateLimitException;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
@@ -34,23 +34,19 @@ public class RateLimitAop {
 
     @Before("@annotation(com.example.annotation.RateLimit)")
     public void before(JoinPoint joinPoint) {
-        Class<?> aClass = joinPoint.getSignature().getDeclaringType();
-        for (Method method : aClass.getMethods()) {
-            if (ObjectUtil.equal(joinPoint.getSignature().getName(), method.getName())) {
-                if (method.isAnnotationPresent(com.example.annotation.RateLimit.class)) {
-                    RateLimit rateLimit = method.getAnnotation(RateLimit.class);
-                    String methodName = method.getName();
-                    int limit = rateLimit.limit();
-                    int token = tokens.getOrDefault(methodName, 0);
-                    //因为不清楚每个接口各自的令牌数量，所以为了方便重置令牌线程，这里从小到大统计
-                    if (token < limit) {
-                        //令牌桶中还有令牌，加上本次请求消耗的令牌
-                        tokens.put(methodName, token + 1);
-                    } else {
-                        //令牌桶中没有令牌，抛出异常
-                        throw new RateLimitException("Rate Limit Exception");
-                    }
-                }
+        Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
+        if (method.isAnnotationPresent(RateLimit.class)) {
+            RateLimit rateLimit = method.getAnnotation(RateLimit.class);
+            String methodName = method.getName();
+            int limit = rateLimit.limit();
+            int token = tokens.getOrDefault(methodName, 0);
+            //因为不清楚每个接口各自的令牌数量，所以为了方便重置令牌线程，这里从小到大统计
+            if (token < limit) {
+                //令牌桶中还有令牌，加上本次请求消耗的令牌
+                tokens.put(methodName, token + 1);
+            } else {
+                //令牌桶中没有令牌，抛出异常
+                throw new RateLimitException("Rate Limit Exception");
             }
         }
     }
