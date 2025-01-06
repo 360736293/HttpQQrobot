223abb49fa998fb3c35eacb3974126fb46f2a23b
360736293 <360736293@qq.com>
2025-01-06 20:00:55 +0800

    fix：修复RequestHolder多线程下的并发问题；

--- src/main/java/com/httpqqrobot/utils/RequestHolder.java
+++ src/main/java/com/httpqqrobot/utils/RequestHolder.java
@@ -1,23 +1,38 @@
 package com.httpqqrobot.utils;
 
+import cn.hutool.core.util.ObjectUtil;
 import com.alibaba.fastjson.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class RequestHolder {
 
     private RequestHolder() {
     }
 
-    private static final ThreadLocal<JSONObject> threadLocal = new ThreadLocal<>();
+    private static final Map<String, ThreadLocal<JSONObject>> threadLocalMap = new HashMap<>();
 
     public static void add(JSONObject json) {
+        ThreadLocal<JSONObject> threadLocal = Optional.ofNullable(
+                threadLocalMap.get(Thread.currentThread().getName())
+        ).orElse(new ThreadLocal<>());
         threadLocal.set(json);
+        threadLocalMap.put(Thread.currentThread().getName(), threadLocal);
     }
 
     public static void remove() {
-        threadLocal.remove();
+        ThreadLocal<JSONObject> threadLocal = threadLocalMap.get(Thread.currentThread().getName());
+        if (ObjectUtil.isNotEmpty(threadLocal)) {
+            threadLocal.remove();
+        }
     }
 
     public static JSONObject get() {
+        ThreadLocal<JSONObject> threadLocal = Optional.ofNullable(
+                threadLocalMap.get(Thread.currentThread().getName())
+        ).orElse(new ThreadLocal<>());
         return threadLocal.get();
     }
 }
