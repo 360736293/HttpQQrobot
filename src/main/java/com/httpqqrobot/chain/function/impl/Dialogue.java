d42ee622d1440b367f342cc22ba41d7d31b85754
360736293 <360736293@qq.com>
2025-01-09 18:13:05 +0800

    fix：修复AI返回文本内容没有清洗干净导致转移字符干扰机器人回复的问题；

--- src/main/java/com/httpqqrobot/chain/function/impl/Dialogue.java
+++ src/main/java/com/httpqqrobot/chain/function/impl/Dialogue.java
@@ -28,7 +28,12 @@ public class Dialogue implements FunctionAct {
             JSONObject aiAnswer = RobotUtil.sendMessageToTongyiqianwen(messageContent);
             String response = aiAnswer.getJSONObject("output").getJSONArray("choices").getJSONObject(0).getJSONObject("message").getString("content");
             //处理返回数据
-            response = response.replaceAll("\n", "");
+            while (response.contains("\n")) {
+                response = response.replace("\n", "");
+            }
+            while (response.contains("\\")) {
+                response = response.replace("\\", "");
+            }
             RobotUtil.groupReply(groupId, messageId, response);
         } catch (Exception e) {
             log.info("对话回复异常: {}", e.getMessage());
