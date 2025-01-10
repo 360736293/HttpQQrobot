e50decc2c652f879ceacf0464204776842749016
360736293 <360736293@qq.com>
2025-01-10 13:03:34 +0800

    update：优化AI对话记忆逻辑，添加删除记忆功能；

--- src/main/java/com/httpqqrobot/chain/function/impl/Dialogue.java
+++ src/main/java/com/httpqqrobot/chain/function/impl/Dialogue.java
@@ -22,42 +22,64 @@ public class Dialogue implements FunctionAct {
     public void act(JSONObject json) {
         try {
             String message = json.getString("message");
+            String[] messageSplit = message.split(" ");
             //不是艾特不回复
-            if (ObjectUtil.notEqual(message.split(" ")[0], "[CQ:at,qq=" + AppConstant.robotQQ + "]")) {
+            if (ObjectUtil.notEqual(messageSplit[0], "[CQ:at,qq=" + AppConstant.robotQQ + "]")) {
                 return;
             }
             String groupId = json.getString("group_id");
-            String messageContent = message.split(" ")[1];
             String messageId = json.getString("message_id");
             String userId = json.getString("user_id");
-            JSONObject aiAnswer = RobotUtil.sendMessageToTongyiqianwen(groupId, userId, messageContent);
-            String response = aiAnswer.getJSONObject("output").getJSONArray("choices").getJSONObject(0).getJSONObject("message").getString("content");
-            //处理返回数据
-            while (response.contains("\n")) {
-                response = response.replace("\n", "");
+            switch (messageSplit[1]) {
+                case "清除记忆":
+                    clearMemory(groupId, messageId, userId);
+                    break;
+                default:
+                    aiTalk(json, groupId, messageId, userId, messageSplit);
             }
-            while (response.contains("\"")) {
-                response = response.replace("\"", "'");
-            }
-            //将用户以及AI回答存起来，作为下一次用户提问时的上下文数据
-            List<AIRequestBody.Message.MessageContent> chatContextList = AppConstant.chatContext.getOrDefault(groupId + "-" + userId, new ArrayList<>());
-            AIRequestBody.Message.MessageContent userMessage = new AIRequestBody().new Message().new MessageContent();
-            AIRequestBody.Message.MessageContent aiMessage = new AIRequestBody().new Message().new MessageContent();
-            userMessage.setRole("user");
-            userMessage.setContent(messageContent);
-            aiMessage.setRole("assistant");
-            aiMessage.setContent(response);
-            chatContextList.add(userMessage);
-            chatContextList.add(aiMessage);
-            if (chatContextList.size() >= AppConstant.tongyiqianwenMaxContextCount) {
-                //达到了上下文上限，清空对话记录
-                chatContextList.clear();
-            }
-            AppConstant.chatContext.put(groupId + "-" + userId, chatContextList);
-            //将AI回答回复给用户
-            RobotUtil.groupReply(groupId, messageId, response);
         } catch (Exception e) {
             log.info("对话回复异常: {}", e.getMessage());
         }
+    }
+
+    public void clearMemory(String groupId, String messageId, String userId) {
+        List<AIRequestBody.Message.MessageContent> chatContextList = AppConstant.chatContext.get(groupId + "-" + userId);
+        if (ObjectUtil.isNotEmpty(chatContextList)) {
+            chatContextList.clear();
+            AppConstant.chatContext.put(groupId + "-" + userId, chatContextList);
+        }
+        RobotUtil.groupReply(groupId, messageId, "记忆已清除");
+    }
+
+    public void aiTalk(JSONObject json, String groupId, String messageId, String userId, String[] messageSplit) {
+        String messageContent = messageSplit[1];
+        JSONObject aiAnswer = RobotUtil.sendMessageToTongyiqianwen(groupId, userId, messageContent);
+        String response = aiAnswer.getJSONObject("output").getJSONArray("choices").getJSONObject(0).getJSONObject("message").getString("content");
+        //处理返回数据
+        while (response.contains("\n")) {
+            response = response.replace("\n", "");
+        }
+        while (response.contains("\"")) {
+            response = response.replace("\"", "'");
+        }
+        //将用户以及AI回答存起来，作为下一次用户提问时的上下文数据
+        List<AIRequestBody.Message.MessageContent> chatContextList = AppConstant.chatContext.getOrDefault(groupId + "-" + userId, new ArrayList<>());
+        if (chatContextList.size() >= AppConstant.tongyiqianwenMaxContextCount) {
+            //达到了上下文上限，清空最早的一组对话记录
+            chatContextList.remove(0);
+            chatContextList.remove(1);
+        }
+        AIRequestBody.Message.MessageContent userMessage = new AIRequestBody().new Message().new MessageContent();
+        AIRequestBody.Message.MessageContent aiMessage = new AIRequestBody().new Message().new MessageContent();
+        userMessage.setRole("user");
+        userMessage.setContent(messageContent);
+        aiMessage.setRole("assistant");
+        aiMessage.setContent(response);
+        chatContextList.add(userMessage);
+        chatContextList.add(aiMessage);
+        //将用户以及AI消息存起来，作为下一次用户提问时的上下文数据
+        AppConstant.chatContext.put(groupId + "-" + userId, chatContextList);
+        //将AI回答回复给用户
+        RobotUtil.groupReply(groupId, messageId, response);
     }
 }
