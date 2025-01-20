e3d5a82281666116e5ba439fa722de997b53109e
360736293 <360736293@qq.com>
2025-01-20 23:59:25 +0800

    update：优化MybatisPlus的CodeGeneratorUtil;

--- src/main/java/com/httpqqrobot/utils/CodeGeneratorUtil.java
+++ src/main/java/com/httpqqrobot/utils/CodeGeneratorUtil.java
@@ -6,57 +6,80 @@ import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
-import com.baomidou.mybatisplus.generator.config.TemplateConfig;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CodeGeneratorUtil {
+
+    static String ip = "xxxx";
+    static String username = "xxxx";
+    static String password = "xxxx";
+    static String author = "xxxx";
+    static String tableName = "xxxx";
+
     public static void main(String[] args) {
-        //1.获取代码生成器的对象
+        //获取代码生成器的对象
         AutoGenerator autoGenerator = new AutoGenerator();
 
         //设置数据库相关配置
         DataSourceConfig dataSource = new DataSourceConfig();
         dataSource.setDriverName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://xxxx:3306/httpqqrobot?useUnicode=true&characterEncoding=utf8&useSSL=false&allowPublicKeyRetrieval=true");
-        dataSource.setUsername("root");
-        dataSource.setPassword("xxxx");
+        dataSource.setUrl("jdbc:mysql://" + ip + ":3306/httpqqrobot?useUnicode=true&characterEncoding=utf8&useSSL=false&allowPublicKeyRetrieval=true");
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        //赋值数据库相关配置
         autoGenerator.setDataSource(dataSource);
 
         //设置全局配置
         GlobalConfig globalConfig = new GlobalConfig();
-        globalConfig.setOutputDir(System.getProperty("user.dir") + "/src/main/java");    //设置代码生成位置
-        globalConfig.setOpen(false);    //设置生成完毕后是否打开生成代码所在的目录
-        globalConfig.setAuthor("xxxx");    //设置作者
-        globalConfig.setFileOverride(true);     //设置是否覆盖原始生成的文件
-        globalConfig.setIdType(IdType.ASSIGN_ID);   //设置Id生成策略
+        //设置生成完毕后是否打开生成代码所在的目录
+        globalConfig.setOpen(false);
+        //设置作者
+        globalConfig.setAuthor(author);
+        //设置是否覆盖原始生成的文件
+        globalConfig.setFileOverride(true);
+        //设置Id生成策略
+        globalConfig.setIdType(IdType.ASSIGN_ID);
+        //赋值全局配置
         autoGenerator.setGlobalConfig(globalConfig);
 
-        //设置包名相关配置
-        PackageConfig packageInfo = new PackageConfig();
-        packageInfo.setParent("com.httpqqrobot");   //设置生成的包名，与代码所在位置不冲突，二者叠加组成完整路径
-        packageInfo.setService("service");  //设置service的包名
-        packageInfo.setServiceImpl("service.impl");  //设置service.impl的包名
-        packageInfo.setMapper("mapper");    //设置mapper的包名
-        packageInfo.setXml("mapper.xml");   //设置mapper.xml的包名
-        packageInfo.setEntity("entity");    //设置entity的包名
-        packageInfo.setController("controller");    //设置controller的包名
-        autoGenerator.setPackageInfo(packageInfo);
+        //设置包相关配置
+        PackageConfig packageConfig = new PackageConfig();
+        Map<String, String> pathInfo = new HashMap<>();
+        //设置生成的包名
+        packageConfig.setParent("com.httpqqrobot");
+        //设置controller的包生成位置
+//        pathInfo.put("controller_path", System.getProperty("user.dir") + "/src/main/java/com/httpqqrobot/controller");
+        //设置service的包生成位置
+        pathInfo.put("service_path", System.getProperty("user.dir") + "/src/main/java/com/httpqqrobot/service");
+        //设置service.impl的包生成位置
+        pathInfo.put("service_impl_path", System.getProperty("user.dir") + "/src/main/java/com/httpqqrobot/service/impl");
+        //设置mapper的包生成位置
+        pathInfo.put("mapper_path", System.getProperty("user.dir") + "/src/main/java/com/httpqqrobot/mapper");
+        //设置mapper.xml的包生成位置
+        pathInfo.put("xml_path", System.getProperty("user.dir") + "/src/main/resources/mapper");
+        //设置entity的包生成位置
+        pathInfo.put("entity_path", System.getProperty("user.dir") + "/src/main/java/com/httpqqrobot/entity");
+        //赋值包名相关配置
+        packageConfig.setPathInfo(pathInfo);
+        autoGenerator.setPackageInfo(packageConfig);
 
-        //策略设置
+        //设置策略配置
         StrategyConfig strategyConfig = new StrategyConfig();
-        strategyConfig.setInclude("xxxx");  //设置当前参与生成的表名，参数为可变参数
-        strategyConfig.setNaming(NamingStrategy.underline_to_camel);  //数据库表名的下划线转换为驼峰
-        strategyConfig.setColumnNaming(NamingStrategy.underline_to_camel);  //数据库表的列名下划线转换为驼峰
-        strategyConfig.setEntityLombokModel(true);  //设置是否启用lombok
+        //设置当前参与生成的表名，参数为可变参数
+        strategyConfig.setInclude(tableName);
+        //数据库表名的下划线转换为驼峰
+        strategyConfig.setNaming(NamingStrategy.underline_to_camel);
+        //数据库表的列名下划线转换为驼峰
+        strategyConfig.setColumnNaming(NamingStrategy.underline_to_camel);
+        //设置是否启用lombok
+        strategyConfig.setEntityLombokModel(true);
+        //赋值策略配置
         autoGenerator.setStrategy(strategyConfig);
 
-        //配置模板
-        TemplateConfig templateConfig = new TemplateConfig();
-        //不生成controller
-        templateConfig.setController("");
-        autoGenerator.setTemplate(templateConfig);
-
-        //2.执行生成操作
+        //执行生成操作
         autoGenerator.execute();
     }
 }
